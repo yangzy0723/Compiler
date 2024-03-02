bdbe918b204b4dbbab2593b31423a8e63b6233c4
JOJO Young <211240045@smail.nju.edu.cn>
2024-03-02 18:28:29 +0800

    finish lab0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
     public static void main(String[] args){
-        if(args.length > 0)
-            System.out.println(args[0]);
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line); // 打印到输出流
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
     }
 }
