061326b8b8f61114923f0274cb793a1acd77f649
JOJO Young <211240045@smail.nju.edu.cn>
2024-03-07 10:31:27 +0800

    improve code

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main
 {    
-    public static void main(String[] args){
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line); // 打印到输出流
-            }
-        } catch (IOException e) {
-            System.err.println("An error occurred: " + e.getMessage());
+    public static void main(String[] args) throws IOException {
+        InputStream is = System.in;
+
+        String file;
+        if(args.length > 0){
+            file = args[0];
+            is = new FileInputStream(file);
         }
+
+        int data;
+        while ((data = is.read()) != -1)
+            System.out.print((char) data);
+
+        is.close();
     }
 }
