f39048bcaeb0a7f69c3ad89ef4aa1f06a8331ed6
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-17 12:17:22 +0800

    > make 211240045 jojo Linux yangzy0723-laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:17:22 up  2:30,  1 user,  load average: 1.99, 1.49, 1.08 902edda2642d037168ce01d3936f0a1cfdd5c713

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-import ant
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
 import java.io.IOException;
 import java.util.List;
 
--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -1,6 +1,6 @@
+import org.antlr.v4.codegen.model.Recognizer;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
 
 public class MyErrorListener extends BaseErrorListener {
     private boolean lexerError = false;
