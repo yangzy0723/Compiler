e7fd90b3025b06960817145890b5bc421aeda580
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-05-30 13:17:52 +0800

    > make 211240045 jojo Linux yangzy-ubuntulaptop 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:52 up 1 day,  3:21,  1 user,  load average: 0.68, 0.32, 0.22 dc23b0ce81af4c8779221543793a46e2d250e1b5

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,10 @@ public class Main {
             System.err.println("input path is required");
         if (args.length < 2)
             System.err.println("output path is required");
-//        String source = "tests/test1.sysy";
-//        String target = "tests/out.ll";
-        String source = args[0];
-        String target = args[1];
+        String source = "tests/test1.sysy";
+        String target = "tests/out.ll";
+//        String source = args[0];
+//        String target = args[1];
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
 
